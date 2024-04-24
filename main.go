40b47a1165dd362377266a0bfe9eab43aec662a5
Igor Jerosimic <igor.jerosimic@checkout.com>
2024-04-24 23:07:49 +0100

    Work in progress

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type InMemorySessionStore struct {
+	data map[string]string
+}
+
+func (i *InMemorySessionStore) Get(key string) (value string) {
+	return i.data[key]
+}
+
+func (i *InMemorySessionStore) Set(key string, value string) {
+	i.data[key] = value
+}
+
+func (i *InMemorySessionStore) GetSession(sessionId string) (session string) {
+	return i.Get(sessionId)
+}
+
+func main() {
+	store := &InMemorySessionStore{
+		data: make(map[string]string)}
+	store.Set("session_1", "session_1")
+	handler := &SessionServer{&InMemorySessionStore{}}
+	log.Fatal(http.ListenAndServe(":5000", handler))
+}
